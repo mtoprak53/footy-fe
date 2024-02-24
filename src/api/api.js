ba60586c4a2c3e44bd474661f25d0432898b7d78
mete <25986959+mtoprak53@users.noreply.github.com>
2024-02-24 05:54:10 -0500

    ENV fix 2

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = import.meta.env.REACT_APP_BASE_URL || "http://localhost:3001";
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = import.meta.env.REACT_APP_BASE_URL || "http://localhost:3001";
+// const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 console.debug("BASE_URL: ", BASE_URL);
 /** API Class.
